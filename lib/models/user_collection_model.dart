aa43f5cdba64ccf4c5e1002dffed701a4ef48780
thanhnv02 <thanhnv02@mic.vn>
2024-06-04 17:33:14 +0700

    add remove don't use file

--- lib/models/user_collection_model.dart
+++
@@ -1,7 +0,0 @@
-// import 'package:demo_deep_link/models/user_model.dart';
-// import 'package:json/json.dart';
-//
-// @JsonCodable()
-// class UserCollectionModel {
-//   final List<UserModel> userList;
-// }
